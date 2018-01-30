31b37b6c81529750af36c797a2c3ca57078f6974
student <student@DEV2-14>
2018-01-30 10:09:21 +0900

    a

--- チーム製作　あんぱんオリジナルゲーム/ObjEnemy.cpp
+++ チーム製作　あんぱんオリジナルゲーム/ObjEnemy.cpp
@@ -56,7 +56,7 @@ void CObjEnemy::Action()
 	{
 		m_move = true;
 	}
-	if (m_hit_right)
+	if (m_hit_right == true)
 	{
 		m_move = false;
 	}
@@ -138,8 +138,8 @@ void CObjEnemy::Draw()
 
 	//切り取り位置の設定
 	src.m_top = 0.0f;
-	src.m_left = 10.0f;	// + AniData[m_ani_frame] * 64;
-	src.m_right = 180.0f;	// + AniData[m_ani_frame] * 64;
+	src.m_left = 10.0f + AniData[m_ani_frame] * 64;
+	src.m_right = 180.0f + AniData[m_ani_frame] * 64;
 	src.m_bottom = src.m_top + 220.0f;
 
 	//ブロック情報を持ってくる
